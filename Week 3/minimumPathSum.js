5bbcbd2ad89f9089e9eb5d390812f948b457c926
Mike Doyle <mikedoyle007@gmail.com>
2020-04-19 22:43:30 -0700

    Solve minPathSum

---
+++ Week 3/minimumPathSum.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var minPathSum = function(grid) {
+  if (grid === null || grid.length === 0) {
+    return 0;
+  }
+
+  let dp = new Array(grid.length).fill(0);
+  for (let i = 0; i < dp.length; i++) {
+    dp[i] = new Array(grid[i].length).fill(0);
+  }
+
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      dp[i][j] += grid[i][j];
+      if (i > 0 && j > 0) {
+        dp[i][j] += Math.min(dp[i-1][j], dp[i][j-1]);
+      } else if (i > 0) {
+        dp[i][j] += dp[i-1][j];
+      } else if (j > 0) {
+        dp[i][j] += dp[i][j-1];
+      }
+    }
+  }
+
+  return dp[dp.length-1][dp[0].length-1];
+};

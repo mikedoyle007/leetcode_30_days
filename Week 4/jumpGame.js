6c97f6a27d13ec91e2af470d68f275d5700a2c1b
Mike Doyle <mikedoyle007@gmail.com>
2020-04-25 22:54:11 -0700

    Solve jumpGame

---
+++ Week 4/jumpGame.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canJump = function(nums) {
+  const length = nums.length;
+  if (!nums || length === 0) {
+    return false;
+  }
+
+  let max = 0;
+
+  for (let i = 0; i < length; i++) {
+    if (nums[i] >= max) {
+      max = nums[i];
+    } else {
+      max--;
+
+      if (max < 0) {
+        return false;
+      }
+    }
+
+    if (nums[i] === 0 && max === 0 && i !== length -1) {
+      return false;
+    }
+  }
+
+  // made it to the end
+  return true;
+};

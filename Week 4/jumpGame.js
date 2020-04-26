8f3ee417a2f97ed77346214713b3b3125246ed6b
Mike Doyle <mikedoyle007@gmail.com>
2020-04-25 22:59:06 -0700

    Update jumpGame

--- Week 4/jumpGame.js
+++ Week 4/jumpGame.js
@@ -11,17 +11,9 @@ var canJump = function(nums) {
   let max = 0;
 
   for (let i = 0; i < length; i++) {
-    if (nums[i] >= max) {
-      max = nums[i];
-    } else {
-      max--;
+    max = Math.max(nums[i], max - 1);
 
-      if (max < 0) {
-        return false;
-      }
-    }
-
-    if (nums[i] === 0 && max === 0 && i !== length -1) {
+    if (max < 0 || (nums[i] === 0 && max === 0 && i !== length -1)) {
       return false;
     }
   }
@@ -29,3 +21,31 @@ var canJump = function(nums) {
   // made it to the end
   return true;
 };
+
+// var canJump = function(nums) {
+//   const length = nums.length;
+//   if (!nums || length === 0) {
+//     return false;
+//   }
+
+//   let max = 0;
+
+//   for (let i = 0; i < length; i++) {
+//     if (nums[i] >= max) {
+//       max = nums[i];
+//     } else {
+//       max--;
+
+//       if (max < 0) {
+//         return false;
+//       }
+//     }
+
+//     if (nums[i] === 0 && max === 0 && i !== length -1) {
+//       return false;
+//     }
+//   }
+
+//   // made it to the end
+//   return true;
+// };
